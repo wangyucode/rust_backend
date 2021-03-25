9dbb6c149aae73ed2f0e1db64a27c5b826c0f065
wangyu <wangyu@wycode.cn>
2021-03-26 02:20:48 +0800

    use one root service

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-use actix_web::{App, HttpServer, web};
-
+use actix_web::{web, App, HttpServer};
 use routes::{admin, public};
 
-mod routes;
+mod routes {
+    pub mod admin;
+    pub mod public;
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new()
-            .service(
-                // prefixes all resources and routes attached to it...
-                public::routes(web::scope("/rust"))
-            )
-            .service(
-                admin::routes(web::scope("/rust/admin"))
-            )
+        App::new().service(
+            // prefixes all resources and routes attached to it...
+            web::scope("/rust")
+                .service(public::routes(web::scope("/public")))
+                .service(admin::routes(web::scope("/admin")))
+        )
     })
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
